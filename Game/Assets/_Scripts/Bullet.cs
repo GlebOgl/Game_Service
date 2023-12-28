5805b415b37810581c14b8ecc6af625e29642c96
GlebOgl <glebogloblin07@gmail.com>
2023-12-28 16:17:58 +0500

    5 Levels

--- Game/Assets/_Scripts/Bullet.cs
+++ Game/Assets/_Scripts/Bullet.cs
@@ -8,6 +8,8 @@ public class Bullet : MonoBehaviour
     public int damage = 10;
     public GameObject explosion;
     private ParticleSystem boltPartSys;
+    private SphereCollider boltCollider;
+    private float timeLeft = 0.2f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +18,21 @@ public class Bullet : MonoBehaviour
         var boltPart = transform.GetChild(0);
         boltPartSys = boltPart.GetComponent<ParticleSystem>();
         boltPartSys.startSpeed = speed;
+
+        var boltColl = transform.GetChild(1);
+        boltCollider = boltColl.GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        timeLeft -= Time.deltaTime;
+        if ( timeLeft < 0 )
+        {
+            boltCollider.enabled = true;
+        }
 
-        transform.position = transform.position + new Vector3(0, -speed * Time.deltaTime, 0);
+        transform.position = transform.position + transform.forward * speed * Time.deltaTime; //new Vector3(0, -speed * Time.deltaTime, 0);
 
         if (transform.position.y < -20.2)
         {
--- Game/Assets/_Scripts/EnemyDragon.cs
+++ Game/Assets/_Scripts/EnemyDragon.cs
@@ -14,6 +14,13 @@ public class EnemyDragon : MonoBehaviour
     public float chanceDirection = 0.1f;
     public int reward = 10;
     public int health = 100;
+    private GameObject Player;
+    public int boltsPerShot = 1;
+    public float spread = 20;
+    public bool isMainDragon = true;
+    public float dragonOffset = 0f;
+
+    public bool autoAIM = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +30,27 @@ public class EnemyDragon : MonoBehaviour
 
     void Shoot(){
         Vector3 myVector = new Vector3(0.0f, 5.0f, 0.25f);
-        GameObject bolt = Instantiate<GameObject>(fireboltPrefab);
-        bolt.transform.position = transform.position + myVector;
+        float shootAngle = 0;
+        float angleChange = 0;
+        if (boltsPerShot > 1){
+            shootAngle = -spread / 2;
+            angleChange = spread / (boltsPerShot - 1);
+        }
+
+        for (int i = 0; i < boltsPerShot; i++)
+        {
+            GameObject bolt = Instantiate<GameObject>(fireboltPrefab);
+            bolt.transform.position = transform.position + myVector;
+
+            if (autoAIM)
+            {
+                bolt.transform.LookAt(Player.transform.position);
+
+            }
+            bolt.transform.localEulerAngles += new Vector3(shootAngle, 0, 0);
+            shootAngle += angleChange;
+        }
+        
         Invoke("Shoot", timeBetweenShoots);
     }
 
@@ -38,18 +64,23 @@ public class EnemyDragon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!Player)
+            Player = GameObject.FindGameObjectWithTag("Player");
         if (health <= 0){
-            SceneManager.LoadScene("_2Scene");
+            if (isMainDragon)
+                SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1)%6);
+            else
+                Destroy(this.gameObject);
         }
 
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
         transform.position = pos;
 
-        if (pos.x < -leftRightDistance){
+        if (pos.x < -(leftRightDistance + dragonOffset)){
             speed = Mathf.Abs(speed);
         }
-        else if (pos.x > leftRightDistance){
+        else if (pos.x > leftRightDistance - dragonOffset){
             speed = -Mathf.Abs(speed);
         }
 
